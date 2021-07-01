19b424a14b9fce9647dea006493963b5a7666439
Varsha1018 <76804038+Varsha1018@users.noreply.github.com>
2021-07-01 17:44:58 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+
+function Bedroom(){
+    window.location="Bedroom.html";
+}
+
+function ac(){
+    window.location="T.V & A.C.html";
+}
+
+function desk(){
+    window.location="desk.html";
+}
+
+function o(){
+    window.location="bottles.html";
+}
+
+function fruit(){
+    window.location="fruit.html";
+}
